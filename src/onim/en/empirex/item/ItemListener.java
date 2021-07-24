16af1e36a488636a75146d15667f4d594c1b7e5f
Oni-Men <sensyaheis@gmail.com>
2021-07-24 23:36:59 +0900

    Relate to some previous changes

--- src/onim/en/empirex/item/ItemListener.java
+++ src/onim/en/empirex/item/ItemListener.java
@@ -8,17 +8,27 @@ import org.bukkit.event.inventory.CraftItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerItemHeldEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.CraftingInventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.projectiles.ProjectileSource;
 
 import com.google.common.base.Strings;
 
 import onim.en.empirex.EmpireX;
+import onim.en.empirex.event.civilian.item.HeldEvent;
+import onim.en.empirex.event.civilian.item.HeldEvent.HeldAction;
 import onim.en.empirex.item.interfaces.Droppable;
 import onim.en.empirex.item.interfaces.Fishable;
-import onim.en.empirex.item.interfaces.Interactable;
+import onim.en.empirex.item.interfaces.Heldable;
+import onim.en.empirex.item.interfaces.LeftClickable;
+import onim.en.empirex.item.interfaces.MoveDetectable;
+import onim.en.empirex.item.interfaces.RightClickable;
 import onim.en.empirex.item.interfaces.Shootable;
+import onim.en.empirex.item.interfaces.ToggleSneakDetectable;
 
 public class ItemListener implements Listener {
 
@@ -47,8 +57,21 @@ public class ItemListener implements Listener {
     ItemStack stack = event.getItem();
     CustomItem customItem = EmpireX.itemFactory.toCustomItem(stack);
 
-    if (customItem instanceof Interactable) {
-      ((Interactable) customItem).onInteract(event);
+    switch (event.getAction()) {
+      case LEFT_CLICK_AIR:
+      case LEFT_CLICK_BLOCK:
+        if (customItem instanceof LeftClickable) {
+          ((LeftClickable) customItem).onLeftClick(event);
+        }
+        break;
+      case RIGHT_CLICK_AIR:
+      case RIGHT_CLICK_BLOCK:
+        if (customItem instanceof RightClickable) {
+          ((RightClickable) customItem).onRightClick(event);
+        }
+        break;
+      default:
+        break;
     }
   }
 
@@ -79,6 +102,50 @@ public class ItemListener implements Listener {
       }
 
     }
-    
+  }
+
+  @EventHandler
+  public void on(PlayerItemHeldEvent event) {
+    Player player = event.getPlayer();
+    PlayerInventory inventory = player.getInventory();
+
+    ItemStack newItem = inventory.getItem(event.getNewSlot());
+    ItemStack prevItem = inventory.getItem(event.getPreviousSlot());
+
+    CustomItem newCustomItem = EmpireX.itemFactory.toCustomItem(newItem);
+    CustomItem prevCustomItem = EmpireX.itemFactory.toCustomItem(prevItem);
+
+    EmpireX.civilianFactory.executeIfPresent(player, c -> {
+      if (newCustomItem instanceof Heldable) {
+        ((Heldable) newCustomItem).onHeld(new HeldEvent(c, HeldAction.START, newItem, prevItem));
+      }
+
+      if (prevCustomItem instanceof Heldable) {
+        ((Heldable) prevCustomItem).onHeld(new HeldEvent(c, HeldAction.STOP, newItem, prevItem));
+      }
+    });
+
+  }
+  
+  @EventHandler
+  public void on(PlayerMoveEvent event) {
+    Player player = event.getPlayer();
+    ItemStack stack = player.getInventory().getItemInMainHand();
+    CustomItem customItem = EmpireX.itemFactory.toCustomItem(stack);
+
+    if (customItem instanceof MoveDetectable) {
+      ((MoveDetectable) customItem).onMove(event);
+    }
+  }
+
+  @EventHandler
+  public void on(PlayerToggleSneakEvent event) {
+    Player player = event.getPlayer();
+    ItemStack stack = player.getInventory().getItemInMainHand();
+    CustomItem customItem = EmpireX.itemFactory.toCustomItem(stack);
+
+    if (customItem instanceof ToggleSneakDetectable) {
+      ((ToggleSneakDetectable) customItem).onToggleSneak(event);
+    }
   }
 }
