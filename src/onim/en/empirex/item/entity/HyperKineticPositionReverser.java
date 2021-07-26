e6d6b870493ee990570145356995c104df95f492
Oni-Men <sensyaheis@gmail.com>
2021-07-26 23:37:19 +0900

    Update HyperKineticPositionReverser.java

--- src/onim/en/empirex/item/entity/HyperKineticPositionReverser.java
+++ src/onim/en/empirex/item/entity/HyperKineticPositionReverser.java
@@ -4,42 +4,59 @@ import java.util.Arrays;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.util.RayTraceResult;
 
 import net.md_5.bungee.api.ChatColor;
 import onim.en.empirex.item.CustomItem;
 import onim.en.empirex.item.interfaces.RightClickable;
 
-public class HyperKineticPositionReverser extends CustomItem implements RightClickable{
+public class HyperKineticPositionReverser extends CustomItem implements RightClickable {
 
-	public HyperKineticPositionReverser() {
-		super("position_reverser",ChatColor.AQUA+"Hyper-Kinetic Position Reverser",Arrays.asList("right click to swap you with the entity you're looking at."));
-	}
+  public HyperKineticPositionReverser() {
+    super("position_reverser", ChatColor.AQUA + "Hyper-Kinetic Position Reverser", Arrays
+      .asList("Right click to swap you with the entity you're looking at."));
+  }
 
-	@Override
-	public Material getMaterial() {
-		return Material.MUSIC_DISC_CAT;
-	}
+  @Override
+  public Material getMaterial() {
+    return Material.MUSIC_DISC_CAT;
+  }
 
-	@Override
-	public boolean isGlowing() {
-		return true;
-	}
+  @Override
+  public boolean isGlowing() {
+    return true;
+  }
 
-	@Override
-	public void onRightClick(PlayerInteractEvent event) {
-		Player player = event.getPlayer();
-		Entity entity = player.getTargetEntity(16);
-		if(entity == null) return;
-		Location playerlocation = player.getLocation();
-		Location entitylocation = entity.getLocation();
-		player.teleport(entitylocation);
-		entity.teleport(playerlocation);
+  @Override
+  public void onRightClick(PlayerInteractEvent event) {
+    Player player = event.getPlayer();
+    Entity entity = getLookingEntity(player);
 
-	}
+    if (entity == null) {
+      return;
+    }
 
+    Location playerlocation = player.getLocation();
+    Location entitylocation = entity.getLocation();
 
+    player.teleport(entitylocation);
+    entity.teleport(playerlocation);
+  }
 
+  private Entity getLookingEntity(Player player) {
+    World world = player.getWorld();
+    Location location = player.getLocation().add(0, 1, 0);
+    RayTraceResult result = world
+      .rayTraceEntities(location, location.getDirection(), 16, e -> !e.equals(player));
+
+    if (result == null) {
+      return null;
+    }
+
+    return result.getHitEntity();
+  }
 }
